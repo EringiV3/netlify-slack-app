2b2e7dbc71e3bccaaa6fdd9536a6d320c9bb8727
萩原孝大 <kodaihagihara@EringiV3.local>
2019-10-30 00:40:37 +0900

    event.js追記

--- src/event.js
+++ src/event.js
@@ -1,6 +1,26 @@
-exports.handler = (event, context, callback) => {
+import { WebClient } from '@slack/client'
+
+exports.handler = async (event, context, callback) => {
   const body = JSON.parse(event.body)
+  const slackEvent = body.event
   console.log(JSON.stringify(body, null, 4))
+
+  if (
+    slackEvent &&
+    slackEvent.type === 'reaction_added' &&
+    slackEvent.item.type === 'message'
+  ) {
+    const web = new WebClient(process.env.SLACK_TOKEN)
+    const res = await web.conversations.history({
+      latest: slackEvent.item.ts,
+      limit: 1,
+      channel: slackEvent.item.channel,
+      inclusive: true
+    })
+    const message = res.messages[0]
+
+    console.log(JSON.stringify(message, null, 4))
+  }
 
   callback(null, {
     statusCode: 200,
