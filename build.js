16e7a8cde8c4233729c96436efafac10b109c304
萩原孝大 <kodaihagihara@EringiV3.local>
2019-11-03 21:03:27 +0900

    build.js修正

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-const sourceDir = './data'
-const outputPath = './public/messages.json'
+const sourceDir = 'data'
+const outputPath = 'public/messages.json'
 
 let result = {}
 const files = fs.readdirSync(sourceDir)
